840eb6fca3b6e8d26937815e1285bff0c33d73e8
Danish Khan <turing@Danishs-MacBook-Pro.local>
2015-10-09 23:35:38 -0500

    few new modules

---
+++ Python/wordLadderProblem.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import graphModule
+
+def demo():
+	g = graphModule.Graph()
+	for i in range(6):
+		g.addVertex(i)
+
+	print g.vertList
+
+	g.addEdge(0,1,5)
+	g.addEdge(0,5,2)
+	g.addEdge(1,2,4)
+	g.addEdge(2,3,9)
+	g.addEdge(3,4,7)
+	g.addEdge(3,5,3)
+	g.addEdge(4,0,1)
+	g.addEdge(5,2,8)
+	g.addEdge(5,2,1)
+
+	for v in g:
+		for w in v.getConnections():
+			print ("(%s , %s)" % (v.getId(),w.getId()))
+
+
+if __name__ == '__main__':
+	demo()
