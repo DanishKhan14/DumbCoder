7cb3481ca9288298e99b37635f83a82bd1473ed2
Danish Khan <turing@Danishs-MacBook-Pro.local>
2015-10-09 23:29:18 -0500

    queue implementation in python module

---
+++ Python/queueModule.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+class Queue():
+	"""
+	"""
+
+	def __init__(self):
+		self.items = []
+
+	def enqueue(self, item):
+		self.items.insert(0,items)
+
+	def dequeue(self):
+		return self.items.pop()
+
+	def isEmpty(self):
+		return self.items == []
+
+	def size(self):
+		return len(self.items)
+
+
+class Dequeue():
+	"""
+	"""
+	def __init__():
+		self.items = []
+
+	def addFront(self, item):
+		self.items.append(item)
+
+	def addRear(self, item):
+		self.items.insert(0,item)
+
+	def removeFront(self):
+		return self.items.pop()
+
+	def removeRear(self):
+		return self.items.pop(0)
+
+	def size(self):
+		return len(self.items)
+
+	def isEmpty(self):
+		return self.items == []
