de7d65c431fd1d09951b2a876775a78d836b0636
DanishKhan14 <danish.belief@gmail.com>
2015-11-01 23:08:56 -0600

    added legal moves fn

--- Python/knightsTourProblem.py
+++ Python/knightsTourProblem.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+def genLegalMoves(x,y,bdSize):
+    newMoves = []
+    moveOffsets = [(-1,-2),(-1,2),(-2,-1),(-2,1),
+                   ( 1,-2),( 1,2),( 2,-1),( 2,1)]
+    for i in moveOffsets:
+        newX = x + i[0]
+        newY = y + i[1]
+        if legalCoord(newX,bdSize) and \
+                        legalCoord(newY,bdSize):
+            newMoves.append((newX,newY))
+    return newMoves
+
+def legalCoord(x,bdSize):
+    if x >= 0 and x < bdSize:
+        return True
+    else:
+        return False
+
+
 from pythonds.graphs import Graph, Vertex
 def knightTour(n,path,u,limit):
         u.setColor('gray')
