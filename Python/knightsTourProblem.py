22eed8eeb74aab5e2c0eb08e4b2cec20621495a8
DanishKhan14 <danish.belief@gmail.com>
2015-11-01 23:10:06 -0600

    added legal moves fn

--- Python/knightsTourProblem.py
+++ Python/knightsTourProblem.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+
+from pythonds.graphs import Graph, Vertex
+
+def knightGraph(bdSize):
+    ktGraph = Graph()
+    for row in range(bdSize):
+       for col in range(bdSize):
+           nodeId = posToNodeId(row,col,bdSize)
+           newPositions = genLegalMoves(row,col,bdSize)
+           for e in newPositions:
+               nid = posToNodeId(e[0],e[1],bdSize)
+               ktGraph.addEdge(nodeId,nid)
+    return ktGraph
+
+def posToNodeId(row, column, board_size):
+    return (row * board_size) + column
+
 def genLegalMoves(x,y,bdSize):
     newMoves = []
     moveOffsets = [(-1,-2),(-1,2),(-2,-1),(-2,1),
@@ -18,8 +35,6 @@ def legalCoord(x,bdSize):
     else:
         return False
 
-
-from pythonds.graphs import Graph, Vertex
 def knightTour(n,path,u,limit):
         u.setColor('gray')
         path.append(u)
