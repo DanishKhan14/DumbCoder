560d6199cf96c13fb2784185de226c47739b8097
DanishKhan14 <danish.belief@gmail.com>
2016-01-06 20:10:14 -0600

    Ordered and Unordered list

---
+++ Python/LinkedList/linkedList.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+class Node():
+    def __init__(self, key):
+        self.data = key
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,data):
+        self.data = data
+
+    def setNext(self,nextItem):
+        self.next = nextItem
+
+class ULinkedList():
+    '''
+    this is unordered linked list. Here items are added in the beginning
+    '''
+    def __init__(self):
+        self.head = None
+
+    def addNode(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def isEmpty(self):
+        return self.head is None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count +1
+            current = current.getNext()
+        return count
+
+    def search(self, key):
+        current = self.head
+        found = False
+        while current is not None or found is True:
+            if current.getData() == key:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def removeNode(self, key):
+        previous = None
+        current = self.head
+        found = False
+        while current is not None or found is True:
+            if current.getData() == key:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if found:
+            if previous is None:
+                self.head = current.getNext()
+            else:
+                previous.setNext() == current.getNext()
+
+        return found
+
+
+class OLinkedList():
+    '''
+    this is ordered linked list. here elements are ordered based on
+    some condition. in this particular implementatio, we are imple-
+    -menting sorted linked list.
+    '''
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        current = self.head
+        found = False
+        previous = None
+        while current is not None and found is False:
+            if current.getData() >= item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous is None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+    def search(self, key):
+        current = self.head
+        found = False
+        stop = False
+
+        while current is not None and not found and not stop:
+            if current.getData() == key:
+                found = True
+            elif current.getData() > key:
+                stop = True
+            else:
+                current = current.getNext()
+
+        return found
+
+olist = OLinkedList()
+ulist = ULinkedList()
+for i in range(10):
+    ulist.addNode(i)
+
+olist.add(34)
+olist.add(23)
+olist.add(12)
+olist.add(29)
+olist.add(9)
+
+print ulist.size()
+print olist.search(34)
+
