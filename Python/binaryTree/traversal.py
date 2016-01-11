353acbe10cec0c32caf78b75f82c48163c110a9d
DanishKhan14 <danish.belief@gmail.com>
2016-01-11 16:52:26 -0600

    added preorder traversal no recursion

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -28,3 +28,27 @@ InOrder Traversal no recursion
                 l.append(cur.val)
                 cur=cur.right
         return l
+
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        l = []
+        if root is None:
+            return l
+        if root.left is None and root.right is None:
+            l.append(root.val)
+            return l
+        import Queue
+        q = Queue.LifoQueue()
+        cur = root
+        while(cur or not q.empty()):
+            if cur:
+                q.put(cur)
+                l.append(cur.val)
+                cur = cur.left
+            else:
+                cur = q.get()
+                cur = cur.right
+        return l
