e8c12404f6b161dd9ba777e783c91ed30a673ccc
DanishKhan14 <danish.belief@gmail.com>
2016-01-11 16:47:33 -0600

    inorderTraversal no recursion

---
+++ Python/binaryTree/traversal.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+"""
+InOrder Traversal no recursion
+"""
+
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        l = []
+        if root is None:
+            return l
+        if root.left is None and root.right is None:
+            l.append(root.val)
+            return l
+
+        import Queue
+        q = Queue.LifoQueue()
+        cur = root
+        while(cur is not None or not q.empty()):
+            if cur:
+                q.put(cur)
+                cur = cur.left
+            else:
+                cur = q.get()
+                l.append(cur.val)
+                cur=cur.right
+        return l
