1c805e4068f31615a13896e2bf1d7617a75bd20e
DanishKhan14 <danish.belief@gmail.com>
2016-01-11 17:18:13 -0600

    postorder traversal no recursion

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -52,3 +52,48 @@ InOrder Traversal no recursion
                 cur = q.get()
                 cur = cur.right
         return l
+
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        """
+        # Chindi Method
+
+        l = []
+        visited = None
+        if root is None:
+            return l
+        if root.left is None and root.right is None:
+            l.append(root.val)
+            return l
+        import Queue
+        q = Queue.LifoQueue()
+        cur = root
+        while (cur or not q.empty()):
+            if cur:
+                q.put(cur)
+                cur = cur.left
+            else:
+                cur = q.get()
+                if cur.right is None or cur.right == visited:
+                    l.append(cur.val)
+                    visited = cur
+                if not cur == visited:
+                    q.put(cur)
+                cur = cur.right
+        return l
+        """
+
+        """
+        Pelar Method _/\_
+        """
+        ans, stack = [], [root]
+        while stack:
+            tmp = stack.pop()
+            if tmp:
+                ans.append(tmp.val)
+                stack.append(tmp.left)
+                stack.append(tmp.right)
+        return ans[::-1]
