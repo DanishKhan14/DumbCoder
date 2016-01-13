075eaf858e4779218a52d8f2c90717e6b61c0a99
DanishKhan14 <danish.belief@gmail.com>
2016-01-13 03:56:23 -0600

    isBalanced

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -121,3 +121,24 @@ InOrder Traversal no recursion
                 self.val = cur.val
                 cur = cur.right
         return isBST
+
+    def isBalanced(self, root):
+        if self.checkHeight(root) == -1:
+            return False
+        else:
+            return True
+
+    def checkHeight(self, root):
+        if root == None:
+            return 0
+        leftHeight = self.checkHeight(root.left)
+        if leftHeight == -1:
+            return -1
+        rightHeight = self.checkHeight(root.right)
+        if rightHeight == -1:
+            return -1
+        diffHeight = abs(leftHeight - rightHeight)
+        if diffHeight > 1:
+            return -1
+        else:
+            return max(leftHeight, rightHeight) + 1
