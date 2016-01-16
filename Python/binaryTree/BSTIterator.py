1a3c6fe4a247ff1d3574c937493096ae637126a5
DanishKhan14 <danish.belief@gmail.com>
2016-01-16 12:55:33 -0600

    BSTIterator

---
+++ Python/binaryTree/BSTIterator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+class BSTIterator(object):
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.nextVal = None
+        self.stack = []
+        self.root = root
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.root:
+            self.stack.append(self.root)
+            self.root = self.root.left
+        else:
+            if not self.stack:
+                return False
+            temp = self.stack.pop()
+            self.nextVal = temp.val
+            self.root = temp.right
+        return True
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.nextVal
