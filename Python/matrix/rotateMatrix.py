08adb3568e49c32739bb5b8b1797ef2d0b46f314
DanishKhan14 <danish.belief@gmail.com>
2016-01-20 01:03:24 -0600

    In Place Rotation

--- Python/matrix/rotateMatrix.py
+++ Python/matrix/rotateMatrix.py
@@ -10,13 +10,49 @@ dstMatrix[col][m - row - 1] = srcMatrix[row][col]
 
 """
 
-def rotate(self, board, m, n):
+def rotate(self, board):
 
-    dst = [[0 for j in range(m)] for i in range(n)]
+    n = len(board)
+    dst = [[0 for j in range(n)] for i in range(n)]
 
-    for i in range(m):
+    for i in range(n):
         for j in range(n):
-            dst[j][m - i - 1] = board[i][j]
+            dst[j][n - i - 1] = board[i][j]
 
-    return dst
+    for i in range(n):
+        for j in range(n):
+            board[i][j] = dst[i][j]
+
+
+## In Place Solution
+
+"""
+First transpose then reverse
+
+Example:
+1 2 3 --> 1 4 7 --> 7 4 1
+4 5 6 --> 2 5 8 --> 8 5 2
+7 8 9 --> 3 6 9 --> 9 6 3
+
+"""
+
+def inPlaceRotate(self, board):
+
+    n = len(board)
+
+    #Transpose
+    for i in range(n):
+        for j in range(i+1, n):
+            temp = board[i][j]
+            board[i][j] = board[j][i]
+            board[j][i] = temp
+
+    #Reverse
+    for i in range(n):
+        for j in range(n//2):
+            temp = board[i][j]
+            board[i][j] = board[i][n-j-1]
+            board[i][n-j-1] = temp
+
+
 
