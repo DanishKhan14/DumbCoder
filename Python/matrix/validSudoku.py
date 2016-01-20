b19ba294bb036e8f30ca9fa9eff6a3a189b4d7fd
DanishKhan14 <danish.belief@gmail.com>
2016-01-19 22:48:42 -0600

    improved

--- Python/matrix/validSudoku.py
+++ Python/matrix/validSudoku.py
@@ -5,6 +5,42 @@
         :type board: List[List[str]]
         :rtype: bool
         """
+
+        #### Method 1
+
+        for i in range(9):
+            d = set()
+            for j in range(9):
+                x = board[i][j]
+                if x != ".":
+                    if x in d:
+                        return False
+                    d.add(x)
+
+        for j in range(9):
+            d = set()
+            for i in range(9):
+                x = board[i][j]
+                if x != ".":
+                    if x in d:
+                        return False
+                    d.add(x)
+
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                d = set()
+                for a in range(i, i+3):
+                    for b in range(j, j+3):
+                        x = board[a][b]
+                        if x!=".":
+                            if x in d:
+                                return False
+                            d.add(x)
+        return True
+
+        """
+        ####  Method 2
+
         for i in range(0,9):
             row = []
             col = []
@@ -32,3 +68,4 @@
                     else:
                         return False
         return True
+        """
