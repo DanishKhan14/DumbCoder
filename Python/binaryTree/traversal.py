37d8c688cd0b09907daf8c2e8cf279eaa3768348
DanishKhan14 <danish.belief@gmail.com>
2016-01-20 21:03:25 -0600

    iterative min depth

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -167,6 +167,12 @@ InOrder Traversal no recursion
         return maxDepth
 
     def minDepth(self, root):  # Recursive
+        """
+
+        :param self:
+        :param root:
+        :return:
+        """
         if root is None:
             return 0
         if root.left is None and root.right is None:
@@ -177,8 +183,23 @@ InOrder Traversal no recursion
             return 1 + self.minDepth(root.left)
         return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
 
-    def minDepth(self, root):
-
+    def minDepth(self, root): #Iterative
+        """
+        Use Level Order Traversal or BFS
+        :param self:
+        :param root:
+        :return:
+        """
+        depth, level = 0, [root]
+        while level and level[0]:
+            depth += 1
+            temp = []
+            for n in level:
+                if not n.left and not n.right:
+                    return depth
+                temp.extend([kid for kid in (n.left, n.right) if kid])
+            level = temp
+        return depth
 
 
     def isSameTree(self, p, q):
