00d04eeeb393b8781686f321e2d96356d739f21c
DanishKhan14 <danish.belief@gmail.com>
2016-01-25 22:03:52 -0600

    Added function for LCA in a BST

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -634,3 +634,43 @@ InOrder Traversal no recursion
 
 ############################################################
 
+    # Recursive
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+
+        :param self:
+        :param root:
+        :param p:
+        :param q:
+        :return:
+        """
+        if not root or not p or not q:
+            return None
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+
+    # Iterative
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        while root:
+            if max(p.val, q.val) < root.val:
+                root = root.left
+            elif min(p.val, q.val) > root.val:
+                root = root.right
+            else:
+                return root
+        return None
+
+################################################################
