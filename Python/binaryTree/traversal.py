4c86823e21571089261997198ba5f6778f871c04
DanishKhan14 <danish.belief@gmail.com>
2016-01-26 00:20:42 -0600

    Added sorted array to BST

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -674,3 +674,20 @@ InOrder Traversal no recursion
         return None
 
 ################################################################
+
+    # Recursive
+
+    def sortedArrayToBST(self, nums):
+        if nums:
+            mid = len(nums)/2
+            root = TreeNode(nums[mid])
+            root.left = self.sortedArrayToBST(nums[:mid])
+            root.right = self.sortedArrayToBST(nums[mid+1:])
+            return root
+
+    # Iterative
+
+################################################################
+
+
+
