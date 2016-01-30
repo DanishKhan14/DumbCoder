cc46abf93cc77067034fc483735930388ae7c836
DanishKhan14 <danish.belief@gmail.com>
2016-01-29 18:51:03 -0600

    added fn to return cycle entry point

--- Python/LinkedList/detectCycle.py
+++ Python/LinkedList/detectCycle.py
@@ -31,3 +31,41 @@
             if slow == fast:
                 return True
         return False
+
+    def detectAndReturnCycle(self, head):
+        """
+        # This explanation is taken from dashang's post.
+
+        Consider the following linked list, where E is the cylce entry and X, the crossing point of fast and slow.
+        H: distance from head to cycle entry E
+        D: distance from E to X
+        L: cycle length
+                          _____
+                         /     \
+        head_____H______E       \
+                        \       /
+                         X_____/
+
+
+        If fast and slow both start at head, when fast catches slow, slow has traveled H+D and fast 2(H+D).
+        Assume fast has traveled n loops in the cycle, we have:
+        2H + 2D = H + D + L  -->  H + D = nL  --> H = nL - D
+        Thus if two pointers start from head and X, respectively, one first reaches E, the other also reaches E.
+        In my solution, since fast starts at head.next, we need to move slow one step forward in the beginning of part 2
+
+
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else: # if there is an exception, we reach the end and there is no cycle
+            return None
+        while head != slow:
+            slow = slow.next
+            head = head.next
+        return head
