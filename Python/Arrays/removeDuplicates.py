2bf09a4c76e14355fe52660ffa38f4dd5a53ad4a
DanishKhan14 <danish.belief@gmail.com>
2016-01-30 22:56:28 -0600

    remove duplicates in sorted array

--- Python/Arrays/removeDuplicates.py
+++ Python/Arrays/removeDuplicates.py
@@ -1,42 +1,13 @@
-    # Method 1: Very Pythonic
+# Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 
-    def removeElement1(self, nums, val):
+    def removeDuplicates(self, nums):
         """
         :type nums: List[int]
-        :type val: int
         :rtype: int
         """
-        try:
-            while True:
-                nums.remove(val)
-        except:
-            return len(nums)
-
-    # Method 2: More efficient
-
-    def removeElement(self, nums, val):
-        """
-        It scans numbers from the left to the right, one by one.
-        Once it finds a number that equals to elem, it swaps current
-        element with the last element in the array and then dispose the last.
-        """
-        i = 0
-        n = len(nums)
-        while (i < n):
-            if nums[i] == val:
-                nums[i] = nums[n -1]
-                n-=1
-            else:
-                i+=1
-        return n
-
-    # Method 3: Best
-
-    def removeElement3(self, nums, val):
-        if not nums: return 0
-        A = 0
-        for n in nums:
-            if n != val:
-                nums[A] = n
-                A += 1
-        return A
+        l= 0
+        for r in range(1,len(nums)):
+            if nums[l] != nums[r]:
+                l +=1
+                nums[l] = nums[r]
+        return l+1 if nums else 0
