a75e543000cb0f887fe7f0a5ad190eb90e1bebd6
DanishKhan14 <danish.belief@gmail.com>
2016-02-15 22:02:08 -0600

    Update

--- Python/DynamicProgramming/LongestCommonSequence.py
+++ Python/DynamicProgramming/LongestCommonSequence.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 
-def lcs(x, y):
-    n = len(x)
-    m = len(y)
-    table = dict()  # a hashtable, but we'll use it as a 2D array here
+    def lcs(x, y):
+        n = len(x)
+        m = len(y)
+        table = dict()  # a hashtable, but we'll use it as a 2D array here
 
-    for i in range(n+1):     # i=0,1,...,n
-        for j in range(m+1):  # j=0,1,...,m
-            if i == 0 or j == 0:
-                table[i, j] = 0
-            elif x[i-1] == y[j-1]:
-                table[i, j] = table[i-1, j-1] + 1
-            else:
-                table[i, j] = max(table[i-1, j], table[i, j-1])
-
-    # Now, table[n, m] is the length of LCS of x and y.
-
-    # Let's go one step further and reconstruct
-    # the actual sequence from DP table:
+        for i in range(n+1):     # i=0,1,...,n
+            for j in range(m+1):  # j=0,1,...,m
+                if i == 0 or j == 0:
+                    table[i, j] = 0
+                elif x[i-1] == y[j-1]:
+                    table[i, j] = table[i-1, j-1] + 1
+                else:
+                    table[i, j] = max(table[i-1, j], table[i, j-1])
+        return table[m,n]
+    # Reconstruct
 
     def recon(i, j):
         if i == 0 or j == 0:
@@ -35,9 +32,7 @@ def lcs(x, y):
 # Method
 
 def LCS(X, Y):
-    m = len(X)
-    n = len(Y)
-    # An (m+1) times (n+1) matrix
+    m, n = len(X), len(Y)
     C = [[0] * (n + 1) for _ in range(m + 1)]
     for i in range(1, m+1):
         for j in range(1, n+1):
@@ -45,4 +40,4 @@ def LCS(X, Y):
                 C[i][j] = C[i-1][j-1] + 1
             else:
                 C[i][j] = max(C[i][j-1], C[i-1][j])
-    return C
+    return C[-1][-1]
