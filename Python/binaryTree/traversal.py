2a7f006201be2f7557e7cb3ffa312af1c919e61d
DanishKhan14 <danish.belief@gmail.com>
2016-03-01 00:16:14 -0600

    update

--- Python/binaryTree/traversal.py
+++ Python/binaryTree/traversal.py
@@ -349,15 +349,17 @@ InOrder Traversal no recursion
         :rtype: bool
         """
         ## Recursive Solution ##
-        """
+
         if root is None:
             return False
         if root.left is None and root.right is None and root.val == sum:
             return True
         else:
             return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
-        """
+
+
         ### Iterative Solution : DFS ###
+    def hasPathSum2(self, root, sum):
         if root is None:
             return False
         rsum, node = [0], [root]
