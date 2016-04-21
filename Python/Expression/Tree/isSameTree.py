4b859dac19752ec13d51a79ce562d93df44a5eaa
DanishKhan14 <danish.belief@gmail.com>
2016-04-21 13:13:05 -0500

    update

---
+++ Python/Expression/Tree/isSameTree.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+    # Recursive
+
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val!=q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+    # Iterative Methods
+
+    # DFS with stack
+    def isSameTree2(self, p, q):
+        stack = [(p, q)]
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                stack.append((node1.right, node2.right))
+                stack.append((node1.left, node2.left))
+        return True
+
+    # BFS with queue
+    def isSameTree3(self, p, q):
+        queue = [(p, q)]
+        while queue:
+            node1, node2 = queue.pop(0)
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                queue.append((node1.left, node2.left))
+                queue.append((node1.right, node2.right))
+        return True
